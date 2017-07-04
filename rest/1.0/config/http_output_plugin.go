6daab32a15536222af88eb0148f31cdc3b0be297
Zhi Yan Liu <lzy.dev@gmail.com>
2017-07-04 17:26:49 +0800

    [plugin-config] Updates HTTP Output plugin config
    According to the change on easegateway server side by commit [0].
    
    [0] https://github.com/hexdecteam/easegateway/commit/1c656760557e0a5e98759f22e39f370289ea9225

--- rest/1.0/config/http_output_plugin.go
+++ rest/1.0/config/http_output_plugin.go
@@ -4,7 +4,7 @@ type HTTPOutputPluginConfig struct {
 	commonPluginConfig
 	URLPattern               string            `json:"url_pattern"`
 	HeaderPatterns           map[string]string `json:"header_patterns"`
-	RequestBodyIOKey         string            `json:"request_body_io_key"`
+	Close                    bool              `json:"close_body_after_pipeline"`
 	RequestBodyBufferPattern string            `json:"request_body_buffer_pattern"`
 	Method                   string            `json:"method"`
 	TimeoutSec               uint16            `json:"timeout_sec"` // up to 65535, zero means no timeout
@@ -13,6 +13,7 @@ type HTTPOutputPluginConfig struct {
 	CAFile                   string            `json:"ca_file"`
 	Insecure                 bool              `json:"insecure_tls"`
 
+	RequestBodyIOKey  string `json:"request_body_io_key"`
 	ResponseCodeKey   string `json:"response_code_key"`
 	ResponseBodyIOKey string `json:"response_body_io_key"`
 }
